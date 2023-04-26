024c5fa2b824ec626ba9523b85a47314aeaee758
Carlos Pereira <carloshtcursos@gmail.com>
2023-04-25 20:23:11 -0400

    iniciando rota de recuperação de senha

---
+++ src/services/EmailService.js
@@ -0,0 +1,31 @@
+const nodemailer = require('nodemailer')
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_SERVER,
+    port: process.env.SMTP_PORT,
+    secure: true, // true for 465, false for other ports
+    auth: {
+        user: process.env.SMTP_EMAIL, // generated ethereal user
+        pass: process.env.SMTP_SENHA, // generated ethereal password
+    },
+});
+
+
+
+function enviarTokenRecuperarSenha(email, token) {
+  transporter.sendMail({
+    from: '"Api Link - T42" admin@lambdatech.com.br', // sender address
+    to: email, // list of receivers
+    subject: "Recuperação de senha", // Subject line
+    text: "Recuperar senha", // plain text body
+    html: `
+    <div>
+        <a href="http://localhost:3000/recuperar-senha?token=${token}">Clique aqui para recuperar sua senha</a>
+    </div>`, // html body
+  }).then(re => console.log(re))
+}
+
+
+module.exports = {
+    enviarTokenRecuperarSenha
+}
+
