00387c0441fd91be72774b3568874c64d8e3098e
Carlos Pereira <carloshtcursos@gmail.com>
2023-05-25 19:49:10 -0400

    adicionando buscar por ID

--- src/routes/UsuarioRoute.js
+++ src/routes/UsuarioRoute.js
@@ -3,6 +3,7 @@ const { getUsuarioController } = require("../factories/UsuarioFactory")
 function rota(app) {
     const controller = getUsuarioController()
     app.get('/usuarios', (req, res) => controller.buscarTodos(req, res))
+    app.get('/usuarios/:id', (req, res) => controller.buscarPorId(req, res))
     app.post('/usuarios', (req, res) => controller.criarUsuario(req, res))
     app.put('/usuarios/:id', (req, res) => controller.atualizarUsuario(req, res))
     app.delete('/usuarios/:id', (req, res) => controller.deletarUsuario(req, res))
